688b6c9219f90a9a37dfe0a1af3f8f0b090eef64
Louaq <Louaq@users.noreply.github.com>
2024-06-08 07:36:30 +0000

    deploy: a4138af0a26a65138eeeda89287de9c3a89a2a39

---
+++ assets/column_Team_index.md.BHhTqr_J.js
@@ -0,0 +1,8 @@
+import{V as s,a as l,b as m}from"./chunks/theme.N0YX6hI8.js";import{c as r,I as e,w as a,k as t,o as i,j as n}from"./chunks/framework.DGMnJK1L.js";const c=n("pre",null,[n("code",null,`<template #title>
+  Our Team
+</template>
+<template #lead>
+  The development of VitePress is guided by an international
+  team, some of whom have chosen to be featured below.
+</template>
+`)],-1),b=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"page"},"headers":[],"relativePath":"column/Team/index.md","filePath":"column/Team/index.md","lastUpdated":1717832123000}'),d={name:"column/Team/index.md"},g=Object.assign(d,{setup(u){const o=[{avatar:"https://s21.ax1x.com/2024/05/31/pk8svjS.png",name:"Louaq",title:"Creator",links:[{icon:"github",link:"https://github.com/Louaq"}]}];return(p,_)=>(i(),r("div",null,[e(t(m),null,{default:a(()=>[e(t(s),null,{default:a(()=>[c]),_:1}),e(t(l),{members:o})]),_:1})]))}});export{b as __pageData,g as default};
