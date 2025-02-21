41ae3adfa363755916622dd1b5a9884ff03b4012
youllg <747063715@qq.com>
2025-02-21 11:07:53 +0000

    Modify notice

--- docs/.vitepress/theme/components/notice.vue
+++ docs/.vitepress/theme/components/notice.vue
@@ -17,7 +17,7 @@ setTimeout(function () { visible.value = false }, 5000);
         <h3 class="notice-title">网站公告</h3>
 
         <div class="notice-describe">
-            <p>所有内容已删除！！！所以文章正在迁移</p>
+            <p>所有内容已删除！！！</p>
         </div>
 
         <div class="notice-footer">
